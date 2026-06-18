e8c1779c43be6b7a165d4fd8b162dd2b3ab8b9d2
Byron057 <Byronvelastegui07@gmail.com>
2026-06-18 18:41:23 -0500

    menu de opciones MAIN

--- TrabajoMVC/src/trabajomvc/TrabajoMVC.java
+++ TrabajoMVC/src/trabajomvc/TrabajoMVC.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package trabajomvc;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,32 @@ public class TrabajoMVC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // TODO code application logic here
+        while(true){
+            System.out.println("     Trabajo Grupo 8    ");
+            System.out.println("========================");
+            System.out.println("1.Crud Vehiculos");
+            System.out.println("2.");
+            System.out.println("3.");
+            System.out.println("4.Salir");
+            int opc = sc.nextInt();
+            switch(opc){
+                case 1:
+                    //importar crud de vehiculos
+                case 2:
+                    //importar crud
+                case 3:
+                    //importar crud
+                case 4:
+                    System.out.println("Saliendo del Sistema.....");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Ingrese una Opcion Correcta");
+            }
+        }
+        
     }
     
 }
