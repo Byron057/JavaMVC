32e5a685bb3d7e2881bd054fd70d0b8b9d206704
aracely13 <aracelytoapanta08@gmail.com>
2026-06-19 07:59:08 +0200

    aracely 2

--- TrabajoMVC/src/Views/clientes_view.java
+++ TrabajoMVC/src/Views/clientes_view.java
@@ -3,11 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Views;
+import java.util.Scanner;
+import Controllers.clientes_controller;
+import Models.clientes_model;
 
 /**
  *
  * @author Usuario
  */
 public class clientes_view {
+    Scanner sc = new Scanner(System.in);
+    
+    
     
 }
