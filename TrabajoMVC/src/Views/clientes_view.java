47c6f8ab349a020345972f99e76b4ae9bb982466
aracely13 <aracelytoapanta08@gmail.com>
2026-06-19 08:06:40 +0200

    aracely 3

--- TrabajoMVC/src/Views/clientes_view.java
+++ TrabajoMVC/src/Views/clientes_view.java
@@ -3,9 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Views;
-import java.util.Scanner;
 import Controllers.clientes_controller;
 import Models.clientes_model;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +13,12 @@ import Models.clientes_model;
  */
 public class clientes_view {
     Scanner sc = new Scanner(System.in);
+    clientes_controller control = new clientes_controller();
+    int autoincrementablekey = 1;
+
+    public void main() {
+
+        while (true) {
     
     
     
